7eff70bf5a9408ccdfeb7933750d7b944a000fcc
MarcoVernaUSI <tbs.verna@gmail.com>
2025-03-13 14:18:53 +0100

    feat: updated dockerfile

--- backend/src/url_shortener/main.py
+++ backend/src/url_shortener/main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
+import argparse
+import datetime
 
 MONGODB_CONNECTION_STRING = os.environ.get("MONGODB_CONNECTION_STRING")
 
 client = AsyncIOMotorClient(MONGODB_CONNECTION_STRING, uuidRepresentation="standard")
 db = client["url_shortener"]
 
-app = FastAPI()
+def main():
+    parser = argparse.ArgumentParser(description="URL Shortener Tool")
+    args = parser.parse_args()
+
+    print('working')
+
+if __name__ == "__main__":
+    main()
+
+
